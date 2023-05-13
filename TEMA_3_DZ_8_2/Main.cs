bf69a33e2a784e63bf3a8632b733bcbaf35f1c69
Марис <122129172+Maris-ITMO@users.noreply.github.com>
2023-05-13 17:13:16 +0300

    Добавьте файлы проекта.

---
+++ TEMA_3_DZ_8_2/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace TEMA_3_DZ_8_2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            View3D view3D = new FilteredElementCollector(doc)
+                .OfClass(typeof(View3D))
+                .Cast<View3D>()
+                .FirstOrDefault(v => v.ViewType == ViewType.ThreeD && v.Name.Equals("{3D}"));
+
+            var nwcOption = new NavisworksExportOptions();
+
+            doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc", nwcOption);
+
+            return Result.Succeeded;
+        }
+    }
+}
